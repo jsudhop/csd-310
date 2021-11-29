8df1f723bac92a702f13616278c791fa5567bed0
jsudhop <jonsud85@Gmail.com>
2021-11-28 21:59:29 -0600

    Update mysql_test.py

--- module_8/mysql_test.py
+++ module_8/mysql_test.py
@@ -3,7 +3,7 @@ from mysql.connector import errorcode
 
 config = {
     "user": "jsudhop",
-    "password": "thisisapassword",
+    "password": "MyDBPassword1",
     "host": "127.0.0.1",
     "database": "pysports",
     "raise_on_warnings": True
